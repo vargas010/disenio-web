49451f104758fa83efeb9a7e63af075a8b40cc9f
vargas010 <vargascamachojoseedwin@gmail.com>
2022-05-03 21:49:21 -0400

    proyecto2

---
+++ jquery.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $('button').click(function(){
+        $('navegador').slideDown(2000);
+    });
+    /*$("button").click(function(){
+        $("foother").fateIn();
+    });*/
+});
+
+
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="jquery.js"></script>
+    <div class="container">
+                <div class="row">
+                  <div class="col">
+                    Column
+                  </div>
+                  <div class="col">
+                    Column
+                  </div>
+                  <div class="col">
+                    Column
+                  </div>
+                </div>
+              </div>
